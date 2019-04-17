65dacafeb4ba58e33b1c0d5b2dcf623a2e24111d
MohammadHader <mhader@atypon.com>
2019-04-17 17:52:38 +0300

    all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { OutTable, ExcelRenderer } from "./ExcelParser";
+
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cols: [],
+      rows: []
+    };
+  }
+  fileHandler = event => {
+    let fileObj = event.target.files[0];
+
+    //just pass the fileObj as parameter
+    ExcelRenderer(fileObj, (err, resp) => {
+      if (err) {
+        console.log(err);
+      } else {
+        this.setState({
+          cols: resp.cols,
+          rows: resp.rows
+        });
+      }
+    });
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input
+          type="file"
+          onChange={this.fileHandler.bind(this)}
+          style={{ padding: "10px" }}
+        />
+        <OutTable
+          data={this.state.rows}
+          columns={this.state.cols}
+          tableClassName="ExcelTable2007"
+          tableHeaderRowClass="heading"
+        />
       </div>
     );
   }
